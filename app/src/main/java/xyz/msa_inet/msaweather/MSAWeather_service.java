6d0b8880d3bbb665dae201593c37fda3f86a4f50
msa-39 <sergeya.moiseev39@gmail.com>
2017-08-04 18:22:42 +0300

    Ver. 1.0.0 It's REAL WORK!!! + Logining 2017/08/04

--- app/src/main/java/xyz/msa_inet/msaweather/MSAWeather_service.java
+++ app/src/main/java/xyz/msa_inet/msaweather/MSAWeather_service.java
@@ -4,15 +4,16 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -47,8 +48,17 @@ public class MSAWeather_service extends Service {
 
     private SharedPreferences settings;// = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
 
+    PowerManager.WakeLock wl;
+
     @Override
     public void onCreate() {
+
+        PowerManager pm = (PowerManager) getApplicationContext().getSystemService(Context.POWER_SERVICE);
+        //PowerManager.WakeLock
+        wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MSAWEATHER TAG");
+
+        //Осуществляем блокировку
+        wl.acquire();
 
         // The service is being created
         super.onCreate();
@@ -122,7 +132,7 @@ public class MSAWeather_service extends Service {
         Log.i(LOG_TAG, "startTime = "+DTFormat.format(startTime));
         MSALog.wrLog("startTime = "+DTFormat.format(startTime),isLogEnable);
 
-        timer.schedule(timerTask, startTime, (3L*1000L));
+        timer.schedule(timerTask, startTime, (60L*60L*1000L));
 
 //        return super.onStartCommand(intent, flags, startId);
         return mStartMode;
@@ -154,6 +164,10 @@ public class MSAWeather_service extends Service {
 
     @Override
     public void onDestroy() {
+
+        //Разблокируем поток.
+        wl.release();
+
         // The service is no longer used and is being destroyed
         timer.cancel();
         super.onDestroy();
